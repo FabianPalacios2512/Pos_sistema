1d47b160607a5a303e374821d7bbc3a9f7a071c6
FabianPalacios2512 <fabianpalacios2512@gmail.com>
2025-12-14 11:15:37 -0500

    🚀 feat: Migración a CI/CD nativo (eliminado Docker)
    - Eliminado docker-compose.yml y todos los Dockerfiles
    - Eliminado DOCKER_DEPLOYMENT_GUIDE.md y archivos .env.docker
    - Agregado workflow CI/CD para deploy nativo con Nginx + PHP/Laravel
    - Deploy automático en push a main con git pull, composer, npm build y migraciones
    - Servidor limpio: contenedores, imágenes y volúmenes Docker removidos

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-2203335d'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/index.html",
-    "revision": "0.mru633is7jo"
+    "revision": "0.bj56d42vld8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
